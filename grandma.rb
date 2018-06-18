ef7fa142750999bb38382e3bc6b3ed7cf855aaff
Alex Neustein <alex.neustein@gmail.com>
2018-06-18 18:14:53 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,9 @@
 def speak_to_grandma 
 
   name = gets.chomp
-  if name == (name.upcase)
+  if name == "I LOVE YOU GRANDMA!"
     puts "NO, NOT SINCE 1938!"
+  elsif name == (name.upcase)
   else
     puts "HUH?! SPEAK UP, SONNY!"
   end 
